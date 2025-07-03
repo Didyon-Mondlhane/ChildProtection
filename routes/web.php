63c2c4cff741b49bc40356c7cc9c43e4767b4d1f
Didyon-Mondlhane <mondlhanedj5@gmail.com>
2025-07-04 00:12:15 +0200

    refactor: remove rotas para recursos inexistentes

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,6 @@ use App\Http\Controllers\CountryController;
 use App\Http\Controllers\SectorController;
 use App\Http\Controllers\ProhibitedActivityController;
 use App\Http\Controllers\CountryActivityController;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\SavedQueryController;
-use App\Http\Controllers\CommentController;
 use App\Http\Controllers\ComparisonController;
 
 Route::get('/', [ComparisonController::class, 'index']);
@@ -16,9 +13,6 @@ Route::resource('countries', CountryController::class);
 Route::resource('sectors', SectorController::class);
 Route::resource('prohibited_activities', ProhibitedActivityController::class);
 Route::resource('country_activities', CountryActivityController::class);
-Route::resource('categories', CategoryController::class);
-Route::resource('query_saved_results', SavedQueryController::class);
-Route::resource('comments', CommentController::class);
 Route::resource('comparisons', ComparisonController::class);
 
 Route::get('/comparisons/{id}/exports/{format}', [ComparisonController::class, 'export'])
